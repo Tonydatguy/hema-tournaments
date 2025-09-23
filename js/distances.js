2b76bc8b53d8017b553d6b0979a62f151690a717
HDHP <112966261+HDHP@users.noreply.github.com>
2025-09-23 15:05:45 +0100

    added new pictures and got bogged down in js

--- js/distances.js
+++ js/distances.js
@@ -1,6 +1,6 @@
-const R = 6.378e+6;
-function distancefunction(lat0, long0, lat1, long1) {
-    let hav = 1-Math.cos(lat1-lat0)+Math.cos(lat0)*Math.cos(lat1)*(1-Math.cos(long1-long0));
-    let D = 2*R*Math.asin(Math.sqrt(hav/2))
-    return D;
+const R = 6.378e+6; //earths radius
+function distancefunction(lat0, long0, lat1, long1) { //haversine function for distance
+    let hav = 1-Math.cos(lat1-lat0)+Math.cos(lat0)*Math.cos(lat1)*(1-Math.cos(long1-long0)); //doing the havsine
+    let D = 2*R*Math.asin(Math.sqrt(hav/2)) //solving for the distance
+    return D; //returning the distance
 }
